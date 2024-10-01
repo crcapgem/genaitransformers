d544d75cd1ec76b1c866f38ee46d65fe96d25eda
crcapgem <117296271+crcapgem@users.noreply.github.com>
2024-10-01 12:16:35 +0000

    updates to npm package

--- genai_transformers_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
+++ genai_transformers_app/amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -1,4 +1,11 @@
 export type AmplifyDependentResourcesAttributes = {
+  "api": {
+    "genailambda": {
+      "ApiId": "string",
+      "ApiName": "string",
+      "RootUrl": "string"
+    }
+  },
   "auth": {
     "genaitransformersapp90b75b9d": {
       "AppClientID": "string",
@@ -8,6 +15,15 @@ export type AmplifyDependentResourcesAttributes = {
       "UserPoolArn": "string",
       "UserPoolId": "string",
       "UserPoolName": "string"
+    }
+  },
+  "function": {
+    "genaitransformerslambda": {
+      "Arn": "string",
+      "LambdaExecutionRole": "string",
+      "LambdaExecutionRoleArn": "string",
+      "Name": "string",
+      "Region": "string"
     }
   }
 }
